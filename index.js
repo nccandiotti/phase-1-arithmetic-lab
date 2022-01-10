4839e68ca6260e9302ffa86144f381a5304f8aed
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-10 16:55:09 -0500

    commit tree 44f5a884ee69818cd3ccb6399d1b55584d8e8b8e

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
-
+"strict"
 // Write your code here
+
+// function random() {
+//   return Math.floor(Math.random * parseInt(0, 10)) + 1
+//
+// // 1. Create a variable called `multiply` set to an equation that will multiply the variables `num1` and `num2`; the result of the multiplication should be `62`.
+
+// // let num1 = Math.floor(Math.random() * 62) + 1
+// // let num2 = Math.floor(Math.random() * 62) + 1
+
+// // function multiply(num1, num2) {
+// //   let product = num1 * num2
+// //   while (product !== 62) {
+// //     num1 * num2
+// //     if (num1 * num2 === 62) return 62
+// //   }
+// // }
+// // multiply(num1, num2)
+
+// let num1 = Math.floor(Math.random() * 62) + 1
+// let num2 = Math.floor(Math.random() * 62) + 1
+// function multiply(a, b) {
+//   a * b === 62
+//   while (a * b !== 62) {
+//     a = Math.floor(Math.random() * 62) + 1
+//     b = Math.floor(Math.random() * 62) + 1
+//     if (a * b === 62) return a * b
+//   }
+// }
+// multiply(num1, num2)
+
+// // // 2. Create a variable called `random` that will generate a random integer greater
+
+// // let number = (Math.floor(Math.random() * 20) + 1)
+
+// const positiveInteger
+// let randomNumber = Math.trunc(Math.random() * positiveInteger) + 1
+// function random() {
+//   while (randomNumber < 0) {
+//     Math.trunc(Math.random() * 20) + 1
+//   }
+//   if (number > 0) return randomNumber
+// }
+// random()
+
+// 3. Create a variable called `mod` set to an equation that will calculate the
+//    remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+
+// 4. Create a variable called `max` that finds the highest number in a set; the value returned should be `20`
+
+// multiply
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+// random
+const random = Math.floor(Math.random()) + 1
+//remainder
+const num3 = 19
+const num4 = 5
+const mod = num3 % num4
+
+// max
+const max = Math.ceil(Math.max(20))
